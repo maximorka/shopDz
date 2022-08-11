9b42be97536f2ad7ad78a29de231f7cf6c3ff470
maximorka <maximorka@gmail.com>
2022-08-11 22:43:28 +0300

    clear comment

--- src/main/java/productShop/storage/hibernate/HibernateUtil.java
+++ src/main/java/productShop/storage/hibernate/HibernateUtil.java
@@ -1,16 +1,12 @@
 package productShop.storage.hibernate;
 
 import lombok.Getter;
-import org.hibernate.Session;
+
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 import productShop.entity.Action;
 import productShop.entity.Product;
-import productShop.storage.Init.DataBaseInit;
 
-import java.util.List;
-
-import static productShop.storage.Storage.connectionURL;
 
 public class HibernateUtil {
     private static final HibernateUtil INSTANCE;
@@ -38,39 +34,5 @@ public class HibernateUtil {
         sessionFactory.close();
     }
 
-    public static void main(String[] args) {
-        new DataBaseInit().initDB(connectionURL);
-        HibernateUtil util = HibernateUtil.getINSTANCE();
 
-        Session session = util.getSessionFactory().openSession();
-
-        List<Product> from_product = session.createQuery("from Product", Product.class).list();
-        System.out.println("from_product = " + from_product);
-        session.close();
-
-
-//        Session session = util.getSessionFactory().openSession();
-//        Transaction transaction = session.beginTransaction();
-//        Product newProduct = new Product();
-//        newProduct.setName("apple");
-//        newProduct.setPrice(1.345f);
-//        newProduct.setAction_id(BigInteger.valueOf(1));
-//        session.persist(newProduct);
-//        transaction.commit();
-//
-//
-//        session.close();
-
-//        Session session = util.sessionFactory.openSession();
-//
-//        Transaction transaction = session.beginTransaction();
-//
-//        Product ex = session.get(Product.class, 2L);
-//        ex.setName("Mod");
-//        session.persist(ex);
-//
-//
-//        transaction.commit();
-//        session.close();
-    }
 }
